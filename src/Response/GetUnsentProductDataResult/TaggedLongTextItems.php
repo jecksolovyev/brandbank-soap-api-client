9500cb6fcca9a28d4c6c45b56d8fb01ba559b7eb
Erik Fábry <erik.fabry@smeonline.sk>
2019-08-12 09:08:09 +0200

    Add TaggedLongTextItems class to GetUnsentProductDataResult

---
+++ src/Response/GetUnsentProductDataResult/TaggedLongTextItems.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrandbankSOAPAPIClient\Response\GetUnsentProductDataResult;
+
+/**
+ * Class TaggedLongTextItems
+ *
+ * @author Erik Fábry <erik.fabry@gmail.com>
+ */
+class TaggedLongTextItems extends LongTextItems
+{
+
+}
