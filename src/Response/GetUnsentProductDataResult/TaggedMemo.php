eb1e2d68a05041a35a32a6aa4d8c40cbef7d33b1
Erik Fábry <erik.fabry@smeonline.sk>
2019-08-12 09:27:15 +0200

    Add author

--- src/Response/GetUnsentProductDataResult/TaggedMemo.php
+++ src/Response/GetUnsentProductDataResult/TaggedMemo.php
@@ -2,6 +2,11 @@
 
 namespace BrandbankSOAPAPIClient\Response\GetUnsentProductDataResult;
 
+/**
+ * Class TaggedMemo
+ *
+ * @author Erik Fábry <erik.fabry@gmail.com>
+ */
 class TaggedMemo extends Memo
 {
 
